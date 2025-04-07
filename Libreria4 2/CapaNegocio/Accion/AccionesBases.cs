9767af973669446bb380272653efbee54d32e0a8
HenryAConcepcion <hconcepcion2@est.unibe.edu.do>
2025-04-06 21:32:47 -0400

    Add files via upload

---
+++ Libreria4 2/CapaNegocio/Accion/AccionesBases.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos.Database;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public  class AccionesBases
+    {
+        public DbliraryDataContext dbLibContex = new DbliraryDataContext();
+
+        public List<ReportesIncidencias> ListReportesIncidencias() => dbLibContex.ReportesIncidencias.ToList();
+
+        public List<Roles> ListRoles()
+        {
+            return dbLibContex.Roles.ToList();
+        }
+    }
+}
---
+++ Libreria4 2/CapaNegocio/Accion/AccionesConsulta.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using CapaDatos.Database;  
+
+namespace CapaNegocio.Accion
+{
+    internal class AccionesConsulta : AccionesBases
+    {
+        //metodos de Listar
+
+        #region Listados Genericos
+
+        public List<ConfiguracionSistema> ListConfiguracionSistema()
+        {
+            return dbLibContex.ConfiguracionSistema.ToList();
+        }
+
+        public List<HistorialUso> ListHistorialUso()
+        {
+            return dbLibContex.HistorialUso.ToList();
+        }
+    
+
+    public List<Equipos> ListEquipos()
+        {
+            return dbLibContex.Equipos.ToList();
+        }
+
+       public List<Horarios> ListHorarios()
+        {
+            return dbLibContex.Horarios.ToList();
+        }
+
+        public List<TiposSala> ListTiposSala()
+        {
+            return dbLibContex.TiposSala.ToList();
+        }
+       
+        public List<Salas> ListSala()
+        {
+            return dbLibContex.Salas.ToList();
+        }
+
+        public List<Reservaciones> ListReservaciones()
+        {
+            return dbLibContex.Reservaciones.ToList();
+        }
+
+
+        public List<Usuarios> ListUsuarios()
+        {
+            return dbLibContex.Usuarios.ToList();
+        }
+
+        List<ReportesIncidencias> ListReportesIncidencias()
+        {
+            return dbLibContex.ReportesIncidencias.ToList();
+        }
+
+
+        #endregion Listados Genericos
+
+    }
+}
